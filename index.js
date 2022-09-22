0ccee80790ae046644695b5a26b89066c2c3da6b
Vinicius Mello <vinicius.dallavecchia@yahoo.com.br>
2022-09-22 15:56:04 -0300

    done

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import { dbConnect } from "./config/db.config.js";
+import { restaurantRouter } from "./routes/restaurant.routes.js";
 dbConnect();
 
 const app = express();
 
 app.use(express.json());
 
-// CONFIGUREM O SEUS ROTEADORES AQUI!
+app.use("/restaurant", restaurantRouter)
 
 app.listen(Number(process.env.PORT), () => {
   console.log(`Server up at port ${process.env.PORT}`);
